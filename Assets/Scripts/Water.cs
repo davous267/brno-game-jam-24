4eabbfda137b3d5f2d173621c09fac303562ea9e
Radek Mička <micka.r@email.cz>
2024-04-07 16:58:32 +0200

    Almost almost done

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Water : MonoBehaviour
 {
     [SerializeField] float risingRatePerMinute = 1.0f;
+    [SerializeField] float risingIncrement = 1.0f;
+    [SerializeField] float maxRisingRate = 2.0f;
     private float risingRate;
     private float currentYPosiiton = 0;
 
@@ -16,8 +18,14 @@ public class Water : MonoBehaviour
 
     private void Update()
     {
+        if (risingRate < maxRisingRate / 60)
+        {
+            risingRate += Time.deltaTime * (risingIncrement / 60);
+        }
         currentYPosiiton += risingRate * Time.deltaTime;
         transform.position = new Vector3(transform.position.x, currentYPosiiton, transform.position.z);
+
+        Debug.Log("Rising rate:" + risingRate);
     }
 
     private void OnTriggerEnter(Collider other)
